16224e32f0e60d3f08bf78cb3e66410a85f00fcc
hdewett <105822645+hdewett@users.noreply.github.com>
2022-10-02 19:24:27 -0400

    Fixed dog game performance
    also updated grid

--- js/dogGame.js
+++ js/dogGame.js
@@ -1,66 +1,66 @@
+// MAIN
 ball.addEventListener("click",dogGetBall);
 dog.addEventListener("transitionend",dogAnimationEnd);
-dogRunning = false;
-// dog running is set to false here so I can keep track of when he is running
-
 
 // This is the function that moves the ball and makes dog go after it.
 function dogGetBall() {
 	ball.style.animation = "none";
 	ball.innerHTML = "";
-	var x = Math.floor(Math.random() * (innerWidth-100));
-	var y = Math.floor(Math.random() * (innerHeight-100));
+	let x = Math.floor(Math.random() * (innerWidth-120));
+	let y = Math.floor(Math.random() * (innerHeight-120));
 	moveThing(ball,x,y,0,0);
-	dogTurn(x);
-	setTimeout(function() { moveThing(dog,x,y,-60,-130);
-													dogAnimationStart();
-												},500);
+	setTimeout(function() {
+		clearTimeout(dogTimeout);
+		dogGo(x,y,-60,-130);
+	},500);
+}
+
+// This is the funtion that moves an element on the page.
+function moveThing(thing,x,y,xOffset,yOffset) {
+	thing.style.transform = "translate(" + (x+xOffset) + "px," + (y+yOffset) + "px)";
 }
 
 function dogGo(x,y,xOffset,yOffset) {
 	moveThing(dog,x,y,xOffset,yOffset);
 	dogTurn(x);
-	dogAnimationStart();
+	startAnimation();
 }
-// This is the funtion that moves an element on the page.
-function moveThing(thing,x,y,xOffset,yOffset) {
-	thing.style.transform = "translate(" + (x+xOffset) + "px," + (y+yOffset) + "px)";
+
+function startAnimation(){
+	hideThing(dog_head);
+	hideThing(dog_body);
+	dogRun1();
+	dogTimeout = setTimeout(animation,200);
 }
+
+function animation(){
+	dogRun2();
+	dogTimeout = setTimeout(startAnimation,200);
+}
+
 // This is the function that shows the dog walking images while dog
 // is moving.
-function dogAnimationStart() {
-	if (dogRunning == false) {
-		dog_head.style.display = "none";
-		dog_body.style.display = "none";
-		dogRun1();
-		dogInterval1 = setInterval(dogRun2,175);
-		dogInterval2 = setInterval(dogRun1,350);
-		dogRunning = true;
-	}
-}
 
 function dogAnimationEnd(e) {
 	if (e.srcElement == dog) {
-	dog_run1.style.display = "none";
-	dog_run2.style.display = "none";
-	dog_head.style.display = "unset";
-  dog_body.style.display = "unset";
-	clearInterval(dogInterval1);
-	clearInterval(dogInterval2);
-	dogRunning = false;
+		clearTimeout(dogTimeout);
+		hideThing(dog_run1);
+		hideThing(dog_run2);
+		showThing(dog_head);
+		showThing(dog_body);
 	}
 }
 // This is a function that the dogMoveAnimation function uses to rapidly
 // display and hide one dog walking image.
 function dogRun1() {
-	dog_run1.style.display = "unset";
-	dog_run2.style.display = "none";
+	hideThing(dog_run2);
+	showThing(dog_run1);	
 }
 // This is the same as the last function, but it rapidly displays and hides
 // the second dog walking image.
 function dogRun2() {
-	dog_run1.style.display = "none";
-	dog_run2.style.display = "unset";
+	hideThing(dog_run1);
+	showThing(dog_run2);
 }
 // Makes dog turn towards the direction of the click/ball.
 function dogTurn(x) {
@@ -81,29 +81,36 @@ function showGame() {
 	ball.style.transform = "translate("+getX(yellowCircle)+"px,"+getY(yellowCircle)+"px)";
 	ball.innerHTML = "Click Me!";
 	ball.style.animation = "flash 0.7s alternate infinite ease-in-out";
-	gameArea.style.display = "unset";
-	playBall.style.display = "none";
-	custardHead.style.display = "none";
-	custardBody.style.display = "none";
-	yellowCircle.style.display = "none";
-	pageTitle.style.display = "none";
+	showThing(gameArea);
+	hideThing(playBall);
+	hideThing(custardHead);
+	hideThing(custardBody);
+	hideThing(yellowCircle);
+	hideThing(pageTitle);
 	dogGo(getX(ball),getY(ball),-60,-130);
 }
 // This function hides the ball and makes dog return to the top left corner
 // of the page.
 function hideGame() {
-	gameArea.style.display = "none";
-	playBall.style.display = "unset";
-	custardHead.style.display = "unset";
-	custardBody.style.display = "unset";
-	yellowCircle.style.display = "inline-block";
-	pageTitle.style.display = "unset";
+	hideThing(gameArea);
+	showThing(playBall);
+	showThing(custardHead);
+	showThing(custardBody);
+	showThing(pageTitle);
 	dog.style.transform = "translate(0px,0px)";
+	yellowCircle.style.display = "inline-block";
+}
+
+function hideThing(thing){
+	thing.style.display = "none";
+}
+function showThing(thing){
+	thing.style.display = "unset";
 }
 // These are functions that gets the coordinates of a given Element.
 function getX(thing) {
-	return thing.getBoundingClientRect().left + window.scrollX;
+	return thing.getBoundingClientRect().x
 }
 function getY(thing) {
-	return thing.getBoundingClientRect().y + window.scrollY;
+	return thing.getBoundingClientRect().y;
 }
